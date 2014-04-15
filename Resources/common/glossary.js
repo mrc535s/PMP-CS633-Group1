77d558a74a61d585256ccc1c84165c132dc3cfab
Michael Carney <carneyinteractive@yahoo.com>
2014-04-14 23:33:33 -0500

    Glossary Changes
    -Added terms to glossary.
    -Added more to Estimate Costs process

--- Resources/common/glossary.js
+++ Resources/common/glossary.js
@@ -5,6 +5,11 @@ function tv_headers_filter(_args) {
 	// create table view data object
 	var data = [
 		{title:'Actual cost (AC)', hasChild:true, data: 'The realized cost incurred for the work performed on an activity during a specific time period.', test:'common/glossary-terms/glossary-terms', header:'A'},
+		{title:'Activity cost Estimates', hasChild:true, data: 'Cost estimates for each activity within a work package are aggregated to obtain a cost estimate for each work package.', test:'common/glossary-terms/glossary-terms'},
+		{title:'Analogous Estimating', hasChild:true, test:'common/glossary-terms/glossary-terms', hasChild:true, data: 'Analogous cost estimating uses the values such as scope, cost, budget, and duration or measures of scale such as size, weight, and complexity from a previous, similar project as the basis for estimating the same parameter or measurement for a current project. When estimating costs, this technique relies on the actual cost of previous, similar projects as the basis for estimating the cost of the current project. It is a gross value estimating approach, sometimes adjusted for known differences in project complexity.'},
+		{title:'Analytical Techniques', hasChild:true, data: 'Used to choose strategic options related to funding the project.', test:'common/glossary-terms/glossary-terms'},
+		{title:'Basis of Estimates', hasChild:true, data: 'Supporting detail for cost estimates contained in the basis for estimates should specify any basic assumptions dealing with the inclusion or exclusion of indirect or other costs in the project budget.', test:'common/glossary-terms/glossary-terms', header:'B'},
+		{title:'Bottom-Up Estimating', hasChild:true, test:'common/glossary-terms/glossary-terms', hasChild:true, data: 'Bottom-up estimating is a method of estimating a component of work. The cost of individual work packages or activities is estimated to the greatest level of specified detail. The detailed cost is then summarized or “rolled up” to higher levels for subsequent reporting and tracking purposes. The cost and accuracy of bottom-up cost estimating are typically influenced by the size and complexity of the individual activity or work package.'},
 		{title:'Cost baseline', hasChild:true, data: 'The cost baseline is compared with actual results to determine if a change, corrective action, or preventive action is necessary.', test:'common/glossary-terms/glossary-terms', header:'C'},
 		{title:'Cost management plan', hasChild:true, data: 'The cost management plan describes how the project costs will be managed and controlled', test:'common/glossary-terms/glossary-terms'},
 		{title:'Cost variance (CV)', hasChild:true, data: 'The amount of budget deficit or surplus at a given point in time, expressed as the difference between earned value and the actual cost. It is a measure of cost performance on a project.', test:'common/glossary-terms/glossary-terms', equation: "CV= EV − AC"},
@@ -12,9 +17,14 @@ function tv_headers_filter(_args) {
 		{title:'Earned value (EV)', header:'E', hasChild:true, data: 'A measure of work performed expressed in terms of the budget authorized for that work', test:'common/glossary-terms/glossary-terms'},
 		{title:'Earned value management (EVM)', hasChild:true, data: 'A methodology that combines scope, schedule, and resource measurements to assess project performance and progress', test:'common/glossary-terms/glossary-terms'},
 		{title:'Earned value performance', hasChild:true, data: 'Compares the performance measurement baseline to actual schedule and cost performance', test:'common/glossary-terms/glossary-terms'},
+		{title:'Expert Judgment ', hasChild:true, data: 'Expert judgment, guided by experience in an application area, Knowledge Area, discipline, industry, or similar project, aids in determining the budget. Such expertise may be provided by any group or person with specialized education, knowledge, skill, experience, or training.', test:'common/glossary-terms/glossary-terms'},
+		{title:'Funding Limit Reconciliation', hasChild:true, data: 'The expenditure of funds should be reconciled with any funding limits on the commitment of funds for the project. A variance between the funding limits and the planned expenditures will sometimes necessitate the rescheduling of work to level out the rate of expenditures. This is accomplished by placing imposed date constraints for work into the project schedule.', test:'common/glossary-terms/glossary-terms', header: 'F'},
+		{title: 'Human Resource Management Plan', hasChild:true, test:'common/glossary-terms/glossary-terms', hasChild:true, data: 'The human resource management plan provides project staffing attributes, personnel rates, and related rewards/recognition, which are necessary components for developing the project cost estimates.', header: 'H'},
 		{title:'Planned value (PV)', header:'P', hasChild:true, data: 'The authorized budget assigned to scheduled work.', test:'common/glossary-terms/glossary-terms'},
+		{title: 'Project Funding', hasChild:true, data: 'The projected funding requirements for a project based on the projected expenditures from the cost baseline plus anticipated liabilities found in the cost baseline.', test:'common/glossary-terms/glossary-terms'},
 		{title:'Project management software', hasChild:true, data: 'Used to monitor the three EVM dimensions (PV, EV, and AC), to display graphical trends, and to forecast a range of possible final project results.', test:'common/glossary-terms/glossary-terms'},
 		{title:'Reserve Analysis', header: 'R', hasChild:true, data: 'Used to monitor the status of contingency and management reserves for the project to determine if these reserves are still needed or if additional reserves need to be requested.', test:'common/glossary-terms/glossary-terms'},
+		{title:'Risk Register', hasChild:true, data: 'The risk register should be reviewed to consider how to aggregate the risk response costs. Updates to the risk register are included with project document updates.', test:'common/glossary-terms/glossary-terms'},
 		{title:'Schedule performance index (SPI)', header: 'S', hasChild:true, data: 'A measure of schedule efficiency expressed as the ratio of earned value to planned value.', test:'common/glossary-terms/glossary-terms'},
 		{title:'Schedule variance (SV)', hasChild:true, data: 'A measure of schedule performance expressed as the difference between the earned value and the planned value.', test:'common/glossary-terms/glossary-terms'},
 		{title:'Trend analysis', header: 'T', hasChild:true, data: 'Examines project performance over time to determine if performance is improving or deteriorating.', test:'common/glossary-terms/glossary-terms'},
@@ -43,42 +53,19 @@ function tv_headers_filter(_args) {
 	// set filters
 	var index = [
 		{title:'A',index:0},
-		{title:'B',index:5},
-		{title:'C',index:9},
-		{title:'D',index:13},
-		{title:'E',index:19},
-		{title:'F',index:23},
-		{title:'G',index:27},
-		{title:'H',index:30},
-		{title:'I',index:33},
-		{title:'J',index:36},
-		{title:'K',index:41},
-		{title:'L',index:44},
-		{title:'M',index:48},
-		{title:'N',index:52},
-		{title:'O',index:55},
-		{title:'P',index:(data.length -1)}
+		{title:'B',index:4},
+		{title:'C',index:6},
+		{title:'E',index:10},
+		{title:'F',index:13},
+		{title:'H',index:14},
+		{title:'P',index:15},
+		{title:'R',index:18},
+		{title:'S',index:20},
+		{title:'T',index:22},
+		{title:'V',index:24},
+		{title:'W',index:25}
 	];
 	tableview.index = index;
-	var index2 = [
-	{title:'AA',index:0},
-	{title:'BB',index:5},
-	{title:'CC',index:9},
-	{title:'DD',index:13},
-	{title:'EE',index:19},
-	{title:'FF',index:23},
-	{title:'GG',index:27},
-	{title:'HH',index:30},
-	{title:'II',index:33},
-	{title:'JJ',index:36},
-	{title:'KK',index:41},
-	{title:'LL',index:44},
-	{title:'MM',index:48},
-	{title:'NN',index:52},
-	{title:'OO',index:55},
-	{title:'PP',index:(data.length -1)}
-	
-	];
 	// add table view to the window
 	win.add(tableview);
 	return win;
